bf25a536fcf5d63f29daae9745dd16b6d195a36c
brlumar <126432304+brlumar@users.noreply.github.com>
2023-08-10 05:54:45 -0500

    final working app

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # to_do_or_not_to_do
 Daily Todo list using day.js
+
+
+
+# Work Day Scheduler
+
+A simple calendar app for scheduling your work day.
+
+## Table of Contents
+
+- [Description](#description)
+- [Preview](#preview)
+- [Technologies Used](#technologies-used)
+- [Usage](#usage)
+- [Getting Started](#getting-started)
+- [License](#license)
+
+## Description
+
+Work Day Scheduler is a web application designed to help you manage your work day by providing a visual schedule for different hours. It allows you to enter and save tasks for each hour of the day. The schedule layout uses different colors to indicate past, present, and future time blocks.
+
+## Preview
+
+![Work Day Scheduler Preview](screenshot.png)
+
+## Technologies Used
+
+- HTML
+- CSS (Bootstrap framework)
+- JavaScript (jQuery library)
+- [Day.js](https://github.com/iamkun/dayjs) library for date and time manipulation
+
+## Usage
+
+1. The application displays time blocks for different hours of the work day.
+2. Each time block includes a text area to input tasks and a save button.
+3. The background color of the time blocks changes based on whether the time is in the past, present, or future.
+4. To save a task, enter the task description in the textarea for the corresponding time block and click the save button.
+5. Saved tasks will be stored locally and displayed when you revisit the page.
+
+## Getting Started
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+
+![Screanshot of site](image.png)
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var currentDay = $('#currentDay');
 var currentTime = $('#currentTime');
 var button9 = $('#button9');
@@ -24,25 +21,9 @@ var textArea15 =$('#text15')
 var textArea16 =$('#text16')
 var textArea17 =$('#text17')
 
-
-
-var data = ""; //empty string to hold data. may not be used in final due to the need for an array
-
 var row = $('.time-block'); //assigns a variable to the in the to later assign a color class to them based on the current time
-
 var currentHour = dayjs().hour(); //gets the current hour
-
-var textArray =[
-  {box: 9, text: ""},
-  {box: 10, text: ""},
-  {box: 11, text: ""},
-  {box: 12, text: ""},
-  {box: 13, text: ""},
-  {box: 14, text: ""},
-  {box: 15, text: ""},
-  {box: 16, text: ""},
-  {box: 17, text: ""},
-];
+var textArray =[]; //array that holds text before it and after it goes to local storage
 
 
 //Function that sets the current time. 
@@ -52,7 +33,7 @@ setInterval(function () {
 }, 1000);
 
 
-console.log("the current hour:", currentHour);
+console.log("the current hour:", currentHour); //test console
 
 //function to check the hours for comparison against the hours on the scheduler
 function setColor() {
@@ -61,7 +42,7 @@ function setColor() {
     var hour = parseInt($(this).attr("id").split("-")[1]);
     console.log('hour inside of function loop is:', hour);
 
-    if (hour < currentHour) {
+    if (hour < currentHour) { //changes the color of the text area based on the condition
       $(this).addClass("past");
     } else if (hour === currentHour) {
       $(this).addClass("present");
@@ -72,79 +53,72 @@ function setColor() {
 
 }
 
+function saveToStorage(arr){ //moves the array of data to localStorage
+var jasonVal = JSON.stringify(arr);
+localStorage.setItem('todo', jasonVal);
 
-
-
-function getInput(){
-var inputValue = textArea9.val();
-console.log("the text capture in 9 is:", inputValue);
-
-return inputValue;
 }
 
-function getInput2(areaNumber) {
-  var inputSelector = "textArea" + areaNumber;
-  var inputValue = textArea+areaNumber.val();
-  console.log("the inputSelector is", inputSelector)
-  console.log("the text captured is:", inputValue);
+function saveToArray(){ //pull data from local storage and save it to an array
+  var rawData = localStorage.getItem('todo');
+  var parse = JSON.parse(rawData);
 
-  return inputValue;
+  return parse;
 }
-
-
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  function compareCurrentHour() {
-
-
-  }
-
-
-
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
 
 //Start of the program
 setColor(); //runs code to set the color of the text area based on the hour
+textArray = saveToArray(); //puts local storage data into array to print to screen
+console.log('the initial array is: ', textArray);
 
+if (!textArray){
+  textArray =[
+    {box: 9, text: ""},
+    {box: 10, text: ""},
+    {box: 11, text: ""},
+    {box: 12, text: ""},
+    {box: 13, text: ""},
+    {box: 14, text: ""},
+    {box: 15, text: ""},
+    {box: 16, text: ""},
+    {box: 17, text: ""},
+  ];
+} else {
 
-button9.click(function () {
-  console.log("button9 was clicked");
-  textArray[0].text = textArea9.val();
-  console.log('the array says', textArray);
+textArea9.val(textArray[0].text); //fills each text box with content from array that was taken from local storage
+textArea10.val(textArray[1].text); //fills each text box with content from array that was taken from local storage
+textArea11.val(textArray[2].text); //fills each text box with content from array that was taken from local storage
+textArea12.val(textArray[3].text); //fills each text box with content from array that was taken from local storage
+textArea13.val(textArray[4].text); //fills each text box with content from array that was taken from local storage
+textArea14.val(textArray[5].text); //fills each text box with content from array that was taken from local storage
+textArea15.val(textArray[6].text); //fills each text box with content from array that was taken from local storage
+textArea16.val(textArray[7].text); //fills each text box with content from array that was taken from local storage
+textArea17.val(textArray[8].text); //fills each text box with content from array that was taken from local storage
+}
+
+console.log("after startup esle the array is: ", textArray);
+
+button9.click(function () {  //listens for the button to be clicked
+  console.log("button9 was clicked"); //test log
+  textArray[0].text = textArea9.val();  //sets the array at first  index to the value in the text area 
+  console.log('the array says', textArray);  //test log
+  saveToStorage(textArray);   //calls function to say the array to local storage
 });
 
 button10.click(function () {
   console.log("button9 was clicked");
   textArray[1].text = textArea10.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
 button11.click(function () {
   console.log("button11 was clicked");
   textArray[2].text = textArea11.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
   
 });
 
@@ -152,6 +126,7 @@ button12.click(function () {
   console.log("button12 was clicked");
   textArray[3].text = textArea12.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
 
 });
 
@@ -159,29 +134,39 @@ button13.click(function () {
   console.log("button13 was clicked");
   textArray[4].text = textArea13.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
 button14.click(function () {
   console.log("button14 was clicked");
   textArray[5].text = textArea14.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
 button15.click(function () {
   console.log("button15 was clicked");
   textArray[6].text = textArea15.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
 button16.click(function () {
   console.log("button16 was clicked");
   textArray[7].text = textArea16.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
 button17.click(function () {
   console.log("button17 was clicked");
   textArray[8].text = textArea17.val();
   console.log('the array says', textArray);
+  saveToStorage(textArray);
+
 });
 
